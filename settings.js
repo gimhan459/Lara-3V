b7a41b5b9f19c5255c37d0320e5fb72a7c19618b
gimhan459 <gimhandavinda70@gmail.com>
2026-03-25 10:16:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,10 +6,10 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94779062397' : process.env.OWNER_NUMBER, /+94721331332
-    SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID, /𝕃𝔸ℝ𝔸-𝕄𝔻=qFx3AJIS#4nLikhMVIvMlcPnww7o47iT05w_rj4TsCjAMNEiXWKQ
-    GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? '' : process.env.GITHUB_USER_NAME, /gimhan459
-    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN /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,  example - G5OxxdvEbiBPWxm4A0xypQGlyCr4FS267ifz
+    OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94779062397' : process.env.OWNER_NUMBER,//+94721331332
+    SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,//𝕃𝔸ℝ𝔸-𝕄𝔻=qFx3AJIS#4nLikhMVIvMlcPnww7o47iT05w_rj4TsCjAMNEiXWKQ
+    GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? '' : process.env.GITHUB_USER_NAME,//gimhan459
+    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN//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,  example - G5OxxdvEbiBPWxm4A0xypQGlyCr4FS267ifz
 };
 
 /*
